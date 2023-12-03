4d4a67757a365dbb1a1748e3cf1d2e6bbfd41e59
will <86805234+nglug@users.noreply.github.com>
2023-12-03 15:55:05 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-balls = 2
-people = Number(prompt("how many people"))
-total = balls * people
 
-console.log(people)
-
-alert("total ballage: " + total)
 
 document.getElementById("button").onclick = function() {
     button()
