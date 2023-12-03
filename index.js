abc3d5fbaca85c60af652951554779584152977c
will <86805234+nglug@users.noreply.github.com>
2023-12-03 15:47:23 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 balls = 2
 people = Number(prompt("how many people"))
-console.log(people)
 total = balls * people
 
-alert("total ballage", total)
+console.log(people)
+
+alert("total ballage" + total)
