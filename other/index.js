f2664e2d223ec0ac5eab2f1c83aa715b91f3905a
will <86805234+nglug@users.noreply.github.com>
2023-12-03 18:47:01 +0000

    v

--- other/index.js
+++ other/index.js
@@ -1,8 +1,8 @@
 
 
-document.getElementsById("button").onclick = function() {
+// document.getElementsById("button").onclick = function() {
+//     location.href = "pages/math-game.html";
+// }
+document.getElementByClassName("math").onclick = function() {
     location.href = "pages/math-game.html";
-}
-document.getElementByClassName("home").onclick = function() {
-    location.href = "index.html";
 }
