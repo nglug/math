7b8e8f01af7ed36b8b8c069e940fa55379e83f13
will <86805234+nglug@users.noreply.github.com>
2023-12-03 18:04:29 +0000

    Update index.js

--- other/index.js
+++ other/index.js
@@ -1,8 +1,8 @@
 
 
-document.getElementByClass("math-game").onclick = function() {
+document.getElementsByClassName("math-game").onclick = function() {
     location.href = "pages/math-game.html";
 }
-document.getElementByClass("home").onclick = function() {
+document.getElementByClassName("home").onclick = function() {
     location.href = "index.html";
 }
