a601c75f2896065e9e9bdc1ab2b8ff5643cdfd35
will <86805234+nglug@users.noreply.github.com>
2023-12-03 18:50:38 +0000

    s

--- other/index.js
+++
@@ -1,8 +0,0 @@
-
-
-// document.getElementsById("button").onclick = function() {
-//     location.href = "pages/math-game.html";
-// }
-document.getElementsByClassName("math").onclick = function() {
-    location.href = "pages/math-game.html";
-}
